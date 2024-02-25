47ef0852eb0f2542f8f8fc37b66753dfe988018f
Andrey Moskokov <asm2906@gmail.com>
2024-02-25 13:08:18 +0300

    0.1

---
+++ src/constants/index2.js
@@ -0,0 +1,2 @@
+export const API_KEY ='5d8fdcd5432ebd2a13e69c084861d70d'
+export const BASE_URL ='https://api.openweathermap.org/data/2.5/weather'
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import './assets/styles/main.sass'
+
+
+
+createApp(App).mount('#app')
